28c84eb7540356df1b7af7c9a65f163438749de3
nczaban19 <nczaban19@wooster.edu>
2018-04-03 10:47:43 -0400

    Split lisp file into Matlab-readable data

--- final.m
+++ final.m
@@ -12,19 +12,57 @@ for i=2:(length(result)/2)+1
 end
 clear tmp
 
-pattern = "))((";
+data(100, 999) = "NaN";
+pattern = '))((';
 for j=1:length(result)
+    k=1;
     for i=1:length(result{j})
-        str=result{j}{i};
+        str = result{j}{i};
         if strfind(str, pattern)>1
-            for k=length(result{j}):i+1:-1
-                result{j}{i+1} = result{j}{i};
-            end
-            
+            tmp = regexp(str, '))((', 'split');
+            data(j,k) = tmp{1};
+            k=k+1;
+            data(j,k) = tmp{2};
+        else 
+            data(j,k) = str;
         end
-        result{j}{i} = strrep(result{j}{i}, '(', '');
-        result{j}{i} = strrep(result{j}{i}, ')', '');
+        k=k+1;
     end
 end
 
-clear i j k pattern str
+for j=1:100
+    for i=1:1033
+        data(j,i) = strrep(data(j,i), '(', '');
+        data(j,i) = strrep(data(j,i), ')', '');
+    end
+end
+
+clear i j k pattern str result tmp
+
+%% Separate data by chorale
+for j=1:100
+    i=0;
+    k=1;
+    n=true;
+    while n==true
+        k=k+1;
+        if k>length(data)
+            n=false;
+        elseif data(j,k) == "st"
+            i=i+1;
+            k=k+1;
+            chorale(i,1) = data(j,k);
+            k=k+2;
+            chorale(i,2) = data(j,k);
+            k=k+2;
+            chorale(i,3) = data(j,k);
+            k=k+2;
+            chorale(i,4) = data(j,k);
+            k=k+2;
+            chorale(i,5) = data(j,k);
+            k=k+2;
+            chorale(i,6) = data(j,k);
+        end
+    end
+end
+clear n ans i j k
